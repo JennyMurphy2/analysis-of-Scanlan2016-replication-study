a84a455825aac3a41145a847094f14cd1d517404
Jenny Murphy <x00170612@mytudublin.ie>
2024-09-07 14:22:47 +0100

    update RPE analyses
    included ANOVA as per peer reviewer request

--- Scripts/RPE.R
+++ Scripts/RPE.R
@@ -4,6 +4,7 @@ library(rstatix)
 library(TOSTER)
 library(MOTE)
 library(tidyverse)
+library(emmeans)
 
 # Load data 
 
@@ -13,7 +14,7 @@ active_data <- read_csv("active.csv") %>%
 passive_data <- read_csv("passive.csv") %>%
   drop_na()
 
-# There was a data entry error and participant 16 anmd 17 were duplicated for the passive data
+# There was a data entry error and participant 16 and 17 were duplicated for the passive data
 # Therefore removing both these participants from the active and passive data
 
 active_data <- active_data %>%
@@ -26,52 +27,92 @@ passive_data <- passive_data %>%
 
 # Active data descriptives --------------------------------------------------------------------
 
-# Get column means for RPE for each sprint for the active protocol data
+active_rpe <- active_data %>%
+  select(Participant, sprint1_RPE,sprint2_RPE, sprint3_RPE, sprint4_RPE, sprint5_RPE, sprint6_RPE,
+        sprint7_RPE, sprint8_RPE, sprint9_RPE, sprint10_RPE) %>%
+  mutate(condition = "active")
 
-active_RPE_mean <- colMeans(select(active_data, sprint1_RPE, sprint2_RPE, sprint3_RPE, sprint4_RPE, 
-                                       sprint5_RPE, sprint6_RPE, sprint7_RPE,
-                                       sprint8_RPE, sprint9_RPE, sprint10_RPE)) %>%
-  as.data.frame() %>%
-  rownames_to_column('sprint') %>%
-  rename(active_mean = ".") 
+passive_rpe <- passive_data %>%
+  select(Participant, sprint1_RPE,sprint2_RPE, sprint3_RPE, sprint4_RPE, sprint5_RPE, sprint6_RPE,
+         sprint7_RPE, sprint8_RPE, sprint9_RPE, sprint10_RPE) %>%
+  mutate(condition = "passive")
 
-# Calculate the sd for each sprint for the active protocol data
 
-active_RPE_sd <- active_data %>%
-  select(sprint1_RPE, sprint2_RPE, sprint3_RPE, sprint4_RPE, 
-         sprint5_RPE, sprint6_RPE, sprint7_RPE,
-         sprint8_RPE, sprint9_RPE, sprint10_RPE) %>%
-  sapply(sd, na.rm = TRUE) %>%
-  as.data.frame() %>%
-  rownames_to_column('sprint') %>%
-  rename(active_sd = ".")
+rpe_data <- rbind(active_rpe, passive_rpe)
 
-# Passive data descriptives --------------------------------------------------------------------
+anova_data <- rpe_data %>%
+  pivot_longer(cols = sprint1_RPE:sprint10_RPE,
+               names_to = "time",
+               values_to = "rpe") 
 
-# Get column means for RPE for each sprint for the passive protocol data
+# ANOVA Test-----
+# 1 x 3 ANOVA 
 
-passive_RPE_mean <- colMeans(select(passive_data, sprint1_RPE, sprint2_RPE, sprint3_RPE, sprint4_RPE, 
-                              sprint5_RPE, sprint6_RPE, sprint7_RPE,
-                              sprint8_RPE, sprint9_RPE, sprint10_RPE)) %>%
-  as.data.frame() %>%
-  rownames_to_column('sprint') %>%
-  rename(passive_mean = ".") 
+##afex::aov_4(continuous_var ~ group_var + (RM_var|id_var)
 
-# Calculate the sd for each sprint for the passive protocol data
+anova_results <- afex::aov_4(rpe ~ (condition*time|Participant), 
+                             data = anova_data,
+                             anova_table = list(es = "pes"))
+anova_results
 
-passive_RPE_sd <- 
-  select(passive_data, sprint1_RPE, sprint2_RPE, sprint3_RPE, sprint4_RPE, 
-         sprint5_RPE, sprint6_RPE, sprint7_RPE,
-         sprint8_RPE, sprint9_RPE, sprint10_RPE) %>%
-  sapply(sd, na.rm = TRUE) %>%
-  as.data.frame() %>%
-  rownames_to_column('sprint') %>%
-  rename(passive_sd = ".") 
+summary(anova_results)
 
-# Combine for overall descriptives ----------------
 
-RPE_mean <- left_join(active_RPE_mean, passive_RPE_mean, by = "sprint")
-RPE_sd <- left_join(active_RPE_sd, passive_RPE_sd, by = "sprint")
+### Post hoc contrasts ----------------------------------------------------------------------------
 
-overall_RPE_descriptives <- left_join(RPE_mean, RPE_sd, by = "sprint") 
-overall_RPE_descriptives
+emm_data <-
+  emmeans::emmeans(anova_results, ~ rpe, model = "multivariate")
+emm_data
+
+posthocresults <- pairs(emm_data, adjust = "bon") %>%
+  broom::tidy(conf.int = T)
+posthocresults
+
+
+## Resolving assumptions --------
+
+### Normality test -------
+
+anova_data %>% 
+  dplyr::group_by(condition, time) %>% 
+  rstatix::shapiro_test(rpe) # shapiro-wilk test on individual groups
+
+### Outliers check --------
+
+anova_data %>%
+  group_by(condition) %>%
+  identify_outliers(rpe)
+
+anova_data %>%
+  group_by(time) %>%
+  identify_outliers(rpe)
+
+## Plots ---------
+
+anova_data %>% 
+  ggplot(aes(condition, time, rpe)) +  
+  geom_violin(fill = "gray") +
+  geom_boxplot(width = .07,
+               fill = "white") +
+  geom_jitter(position = position_jitter(0.21)) +
+  stat_summary(fun = mean,
+               geom = "point",
+               shape = 18,
+               color = "red",
+               size = 5) +
+  theme_bw()
+
+# Summary descriptives with outlier removed ------
+
+summary <- anova_data %>% 
+  group_by(condition,time) %>%
+  summarize(overall_mean = mean(rpe,na.rm=TRUE),
+            overall_sd = sd(rpe, na.rm = TRUE),
+            count = n())
+
+summary_conditions <- anova_data %>% 
+  group_by(condition) %>%
+  summarize(overall_mean = mean(rpe,na.rm=TRUE),
+            overall_sd = sd(rpe, na.rm = TRUE),
+            count = n())
+
